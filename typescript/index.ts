987a86c15b98ce79eca1e94ee7065f4a427ad2aa
Sam Joyce <42083561+sammyjoyce@users.noreply.github.com>
2026-07-27 10:39:28 +1000

    feat(typescript): add native bindings and package pipeline

---
+++ typescript/index.ts
@@ -0,0 +1,9 @@
+export { Runtime } from "./runtime.js"
+export { toBytes, utf8 } from "./codec.js"
+export type {
+  ActorAddress,
+  ActorDecision,
+  ActorService,
+  BytesLike,
+  RuntimeOptions,
+} from "./types.js"
